135c57ad1154bce44a7b485d3b3539432290ce8d
Christine Contreras <christineec23@yahoo.com>
2021-08-16 14:47:25 -0500

    working

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+
+    # instance method definition (create method in object)
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+
+end
+
+fido = Dog.new
+fido.bark
